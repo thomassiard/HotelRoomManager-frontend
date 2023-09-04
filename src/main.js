75606d5faa95ada0c4d4050bae49e77624d8c3db
Thomas Siard <75135103+thomassiard@users.noreply.github.com>
2023-09-04 15:53:44 +0200

    Popravljena URL baze

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import { registerPlugins } from "@/plugins";
 const app = createApp(App);
 
 // Baza URL-a za axios
-axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = "https://hotel-room-manager-backend.vercel.app";
 
 app.config.globalProperties.$axios = axios;
 
